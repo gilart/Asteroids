a365dbee74179aa5e3e6e1a05c7c421f38c24712
Arek <kotarek22@gmail.com>
2021-09-14 11:22:03 +0200

    Boundaries check

---
+++ Assets/Scripts/Boundaries/Boundaries.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    public Camera MainCamera;
+    public Vector2 screenBounds;
+
+    void Start()
+    {
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+
+        if(viewPos.x < screenBounds.x)//left side
+        {
+            viewPos.x += 2 * -screenBounds.x; 
+        }
+
+        if (viewPos.x > -screenBounds.x)//right side
+        {
+            viewPos.x += 2 * screenBounds.x;
+        }
+
+        if (viewPos.y < screenBounds.y)//bottom side
+        {
+            viewPos.y += 2 * -screenBounds.y;
+        }
+
+        if (viewPos.y > -screenBounds.y)//top side
+        {
+            viewPos.y += 2 * screenBounds.y;
+        }
+
+        transform.position = viewPos;
+    }
+}
